cf0159722646b8c0f48b103d045c6a3df428d8b2
Philip Guedes <philipguedes@gmail.com>
2017-10-16 11:32:31 -0200

    Fix whitespaces

--- api/src/adapters/voip.py
+++ api/src/adapters/voip.py
@@ -3,20 +3,19 @@ from datetime import datetime as dt
 from src.adapters import BaseAdapter
 
 
-BYTES_TO_MEGABITS = 128 * 1024 
+BYTES_TO_MEGABITS = 128 * 1024
 
 
 class VoipAdapter(BaseAdapter):
     def __init__(self):
         BaseAdapter.__init__(self)
         self.__data = None
-        
+
     @property
     def data(self):
         return self.__data
 
     def update_data(self):
-        
         content = self.get_data()
         # updated = arrow.utcnow().timestamp
         updated = None
@@ -41,22 +40,17 @@ class VoipAdapter(BaseAdapter):
         most_recent = max(data, key=int)
         return most_recent, data[most_recent]
 
-
     def update_plot(self):
         data = self.get_data()
         x = []
         y = []
-        
+
         od = collections.OrderedDict(sorted(data.items()))
         for timestamp, elem in od.items():
             x.append(dt.fromtimestamp(timestamp))
             y.append(elem['goodput']/BYTES_TO_MEGABITS)
-        
+
         trace = self.graphics.scatter_trace(x, y)
         path = self.graphics.render_figure([trace], 'voip')
         print('done')
         return path
-    
-        
-
-    
